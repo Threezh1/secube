0762e3dbfb04db39f0ac15eb385f54b18219187b
Threezh1 <makefoxm@qq.com>
2021-04-15 12:06:54 +0800

    完善细节
    1. js文件细节完善
    2. 增加对主题文件的说明

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # secube
 
 secube安全导航地址：https://threezh1.com/secube/index.html
-
 secube是一个安全信息导航，其中包含了各大信息安全论坛以及信息安全相关的最新文章。
 
 ## process
@@ -11,6 +10,15 @@ secube是一个安全信息导航，其中包含了各大信息安全论坛以�
 整个信息流的框架如下图，secube也就是属于数据展示端。
 
 ![image](https://user-images.githubusercontent.com/45116144/114752712-2f9f9500-9d89-11eb-9bc7-5094dc74b7a6.png)
+
+主题使用了：[WebStackPage.github.io](https://github.com/WebStackPage/WebStackPage.github.io) 由于比较懒就把框架改成了从js中生成文章结构。
+
+## problem
+
+1. 信息流暂时还没法用，如果你想用可以安装一下这个仓库里面的pem证书再打开网址就可以收到信息流了。
+   1. windows下参考Burpsuite的证书安装
+   2. mac下双击证书安装，打开钥匙串，双击secube证书，选择全部信任即可
+2. 后续会增加微信文章、twitter的信息流
 
 ## other
 
--- assets/js/website.js
+++ assets/js/website.js
@@ -1,4 +1,5 @@
 
+// 增加对一些方法的支持
 String.prototype.replaceAll = function (exp, newStr) {
     return this.replace(new RegExp(exp, "gm"), newStr);
 };
@@ -22,92 +23,6 @@ String.prototype.format = function(args) {
     return result;
 }
 
-var website_text =
-`
-常用网址|常用网址|BiliBili|https://www.bilibili.com/|国内知名的视频弹幕网站|https://www.bilibili.com/favicon.ico
-常用网址|常用网址|Github|https://github.com/|GitHub is where people build software|https://github.com//favicon.ico
-常用网址|常用网址|谷歌翻译|https://translate.google.cn/|比较好用的翻译工具|https://translate.google.cn/favicon.ico
-常用网址|常用网址|三之博客|https://threezh1.com/|三之的博客|https://threezh1.com/favicon.ico
-安全工具|安全工具|ctf编码|http://ctf.ssleye.com/|CTFcode为CTF比赛人员、程序员提供20多种常用编码|https://ctf.ssleye.com/static/ctf_favicon.ico
-安全工具|安全工具|ceye|http://ceye.io/|Monitor service for security testing|https://ceye.io/static/ceye.icon
-安全工具|安全工具|regex101|https://regex101.com/|Regular expression tester with syntax highlighting|https://regex101.com/favicon.ico
-安全工具|安全工具|regexper|https://regexper.com/|Regular expression visualizer using railroad diagrams|https://regexper.com/favicon.ico
-安全论坛|安全论坛|先知论坛|https://xz.aliyun.com/|先知安全技术社区|https://xz.aliyun.com/static/icon/favicon.ico
-安全论坛|安全论坛|安全客|http://www.anquanke.com/|安全资讯平台|https://p0.ssl.qhimg.com/t01b2a1553a7bc927cb.ico?v=1.2
-安全论坛|安全论坛|Seebug|https://paper.seebug.org/|Paper - 安全技术精粹|https://paper.seebug.org/static/images/favicon.ico
-安全论坛|安全论坛|知识星球|https://wx.zsxq.com/|知识星球是创作者连接铁杆粉丝，实现知识变现的工具。|https://wx.zsxq.com/dweb2/assets/images/favicon_32.ico
-`
-var websites = website_text.split("\n");
-var websites_json = {};
-for (i in websites) {
-    if (websites[i] == "") { continue };
-    var website_dict = websites[i].split("|");
-    if (websites_json.hasOwnProperty(website_dict[0]) == false) {
-        websites_json[website_dict[0]] = {};
-    }
-    if (websites_json[website_dict[0]].hasOwnProperty(website_dict[1]) == false) {
-        websites_json[website_dict[0]][website_dict[1]] = [];
-    }
-    var siteinfo = {
-        "title": website_dict[2],
-        "link": website_dict[3],
-        "introduction": website_dict[4],
-        "logo": website_dict[5],
-    }
-    websites_json[website_dict[0]][website_dict[1]].push(siteinfo);
-}
-
-var main_content_div = document.querySelector("#main-content")
-var foot_element = document.querySelector("#main-content > h4:nth-child(4)")
-
-for (i in websites_json) {
-    // <h4 class="text-gray"><i class="linecons-tag" style="margin-right: 7px;" id="常用推荐"></i>常用推荐</h4>
-    var kind_title_h4 = document.createElement("h4");
-    kind_title_h4.setAttribute("class", "text-gray");
-    kind_title_h4.innerHTML = `<br /><i class="linecons-tag" style="margin-right: 7px;" id="` + i +`"></i>` + i;
-    foot_element.before(kind_title_h4);
-    row_num = 1
-    for (j in websites_json[i]) {
-        var siteinfo = websites_json[i][j]
-        console.log(siteinfo)
-        row_element = document.createElement("div")
-        row_element.setAttribute("class", "row")
-        foot_element.before(row_element)
-        for (k in siteinfo) {
-            var info = siteinfo[k]
-            site_element = document.createElement("div")
-            site_element.setAttribute("class", "col-sm-3")
-            var site_innerHTML = `
-            <div class="xe-widget xe-conversations box2 label-info" onclick="window.open('{link}', '_blank')" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="{link}">
-            <div class="xe-comment-entry">
-                <a class="xe-user-img">
-                    <img data-src="{logo}" class="lozad img-circle" width="40">
-                </a>
-                <div class="xe-comment">
-                    <a href="#" class="xe-user-name overflowClip_1">
-                        <strong>{title}</strong>
-                    </a>
-                    <p class="overflowClip_2">{introduction}</p>
-                </div>
-            </div>
-            </div>
-            `
-            site_innerHTML = site_innerHTML.replace("{link}", siteinfo[k]["link"])
-            site_innerHTML = site_innerHTML.format({
-                title: siteinfo[k]["title"],
-                link: siteinfo[k]["link"],
-                introduction: siteinfo[k]["introduction"],
-                logo: siteinfo[k]["logo"]
-            })
-            site_element.innerHTML = site_innerHTML
-            row_element.appendChild(site_element)
-        }
-    }
-}
-
-br_element = document.createElement("br")
-foot_element.before(br_element)
-
 Date.prototype.format = function(fmt) {
     var o = {
         "M+" : this.getMonth()+1,                 //月份
@@ -129,12 +44,96 @@ Date.prototype.format = function(fmt) {
     return fmt;
 }
 
+// 定义页面网址
+var website_text =
+`
+常用网址|常用网址|BiliBili|https://www.bilibili.com/|国内知名的视频弹幕网站|https://www.bilibili.com/favicon.ico
+常用网址|常用网址|Github|https://github.com/|GitHub is where people build software|https://github.com//favicon.ico
+常用网址|常用网址|谷歌翻译|https://translate.google.cn/|比较好用的翻译工具|https://translate.google.cn/favicon.ico
+常用网址|常用网址|三之博客|https://threezh1.com/|三之的博客|https://threezh1.com/favicon.ico
+安全工具|安全工具|ctf编码|http://ctf.ssleye.com/|CTFcode为CTF比赛人员、程序员提供20多种常用编码|https://ctf.ssleye.com/static/ctf_favicon.ico
+安全工具|安全工具|ceye|http://ceye.io/|Monitor service for security testing|https://ceye.io/static/ceye.icon
+安全工具|安全工具|regex101|https://regex101.com/|Regular expression tester with syntax highlighting|https://regex101.com/favicon.ico
+安全工具|安全工具|regexper|https://regexper.com/|Regular expression visualizer using railroad diagrams|https://regexper.com/favicon.ico
+安全论坛|安全论坛|先知论坛|https://xz.aliyun.com/|先知安全技术社区|https://xz.aliyun.com/static/icon/favicon.ico
+安全论坛|安全论坛|安全客|http://www.anquanke.com/|安全资讯平台|https://p0.ssl.qhimg.com/t01b2a1553a7bc927cb.ico?v=1.2
+安全论坛|安全论坛|Seebug|https://paper.seebug.org/|Paper - 安全技术精粹|https://paper.seebug.org/static/images/favicon.ico
+安全论坛|安全论坛|知识星球|https://wx.zsxq.com/|知识星球是创作者连接铁杆粉丝，实现知识变现的工具。|https://wx.zsxq.com/dweb2/assets/images/favicon_32.ico
+`
+
+// 把网址信息转为json
+var websites = website_text.split("\n");
+var websites_json = {};
+for (i in websites) {
+    if (websites[i] == "") { continue };
+    var website_dict = websites[i].split("|");
+    if (websites_json.hasOwnProperty(website_dict[0]) == false) {
+        websites_json[website_dict[0]] = {};
+    }
+    if (websites_json[website_dict[0]].hasOwnProperty(website_dict[1]) == false) {
+        websites_json[website_dict[0]][website_dict[1]] = [];
+    }
+    var siteinfo = {
+        "title": website_dict[2],
+        "link": website_dict[3],
+        "introduction": website_dict[4],
+        "logo": website_dict[5],
+    }
+    websites_json[website_dict[0]][website_dict[1]].push(siteinfo);
+}
+
+// 根据json生成html
+var foot_element = document.querySelector("#main-content > h4:nth-child(4)");
+for (i in websites_json) {
+    var kind_title_h4_element = document.createElement("h4");
+    kind_title_h4_element.setAttribute("class", "text-gray");
+    kind_title_h4_element.innerHTML = `<br /><i class="linecons-tag" style="margin-right: 7px;" id="` + i +`"></i>` + i;
+    foot_element.before(kind_title_h4_element);
+    for (j in websites_json[i]) {
+        var siteinfo = websites_json[i][j];
+        var row_element = document.createElement("div");
+        row_element.setAttribute("class", "row");
+        foot_element.before(row_element);
+        for (k in siteinfo) {
+            var info = siteinfo[k];
+            var site_element = document.createElement("div");
+            site_element.setAttribute("class", "col-sm-3");
+            var site_innerHTML = `
+            <div class="xe-widget xe-conversations box2 label-info" onclick="window.open('{link}', '_blank')" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="{link}">
+            <div class="xe-comment-entry">
+                <a class="xe-user-img">
+                    <img data-src="{logo}" class="lozad img-circle" width="40">
+                </a>
+                <div class="xe-comment">
+                    <a href="#" class="xe-user-name overflowClip_1">
+                        <strong>{title}</strong>
+                    </a>
+                    <p class="overflowClip_2">{introduction}</p>
+                </div>
+            </div>
+            </div>
+            `
+            site_innerHTML = site_innerHTML.format({
+                title: siteinfo[k]["title"],
+                link: siteinfo[k]["link"],
+                introduction: siteinfo[k]["introduction"],
+                logo: siteinfo[k]["logo"]
+            })
+            site_element.innerHTML = site_innerHTML;
+            row_element.appendChild(site_element);
+        }
+    }
+}
+
+var br_element = document.createElement("br");
+foot_element.before(br_element);
+
+// 从远程服务器获取信息流
 var requestURL = "https://129.211.93.28:8080/api/extract";
 var info = {};
 info["type"] = "TIME";
 info["keyword"] = new Date().format("yyyy-MM-dd");
-//console.log(info)
-var tbody = document.getElementById("info_tbody_1")
+var tbody = document.getElementById("info_tbody_1");
 $.ajax({
     url: requestURL,
     data: JSON.stringify(info),
@@ -142,33 +141,31 @@ $.ajax({
     dataType: "json",
     contentType: "application/json;charset=utf-8",
     success: function(returnData){
-        //console.log(returnData);
         if (returnData.code == 1000) {
-            returnData.extractInfo.reverse()
+            returnData.extractInfo.reverse();
             for (i in returnData.extractInfo) {
-                title = returnData.extractInfo[i]["title"]
-                link = returnData.extractInfo[i]["link"]
-                source = returnData.extractInfo[i]["source"]
-                source_zh = returnData.extractInfo[i]["source_zh"]
+                var title = returnData.extractInfo[i]["title"];
+                var link = returnData.extractInfo[i]["link"];
+                var source = returnData.extractInfo[i]["source"];
+                var source_zh = returnData.extractInfo[i]["source_zh"];
                 var tr = document.createElement("tr");
                 var td_id = document.createElement('td');
-                td_id.setAttribute("scope", "row")
-                td_id.innerHTML = parseInt(i) + 1
+                td_id.setAttribute("scope", "row");
+                td_id.innerHTML = parseInt(i) + 1;
                 var td_title = document.createElement('td');
-                title_lable = document.createElement("a")
-                title_lable.setAttribute("href", link)
-                title_lable.setAttribute("target", "_blank")
-                title_lable.innerHTML = title
-                td_title.appendChild(title_lable)
+                var title_lable = document.createElement("a");
+                title_lable.setAttribute("href", link);
+                title_lable.setAttribute("target", "_blank");
+                title_lable.innerHTML = title;
+                td_title.appendChild(title_lable);
                 var td_source = document.createElement('td');
-                source_lable = document.createElement("a")
-                source_lable.setAttribute("href", source)
-                source_lable.innerHTML = source_zh
-                td_source.appendChild(source_lable)
-                //console.log(returnData.extractInfo[i])
-                tr.appendChild(td_id)
-                tr.appendChild(td_title)
-                tr.appendChild(td_source)
+                var source_lable = document.createElement("a");
+                source_lable.setAttribute("href", source);
+                source_lable.innerHTML = source_zh;
+                td_source.appendChild(source_lable);
+                tr.appendChild(td_id);
+                tr.appendChild(td_title);
+                tr.appendChild(td_source);
                 tbody.appendChild(tr);
             }
         }
@@ -179,6 +176,7 @@ $.ajax({
     }
 });
 
+// 搜索框
 $('input[type=radio]').click(function(event){
     search_links = {
         google: {
@@ -206,8 +204,8 @@ $('input[type=radio]').click(function(event){
             param: "query"
         }
     }
-    form_element = document.querySelector("#main-content > div:nth-child(3) > div > form")
-    form_element.setAttribute("action", search_links[event.currentTarget.id].link)
-    input_element = document.querySelector("#main-content > div:nth-child(3) > div > form > div > input")
-    input_element.setAttribute("name", search_links[event.currentTarget.id].param)
+    var form_element = document.querySelector("#main-content > div:nth-child(3) > div > form");
+    form_element.setAttribute("action", search_links[event.currentTarget.id].link);
+    var input_element = document.querySelector("#main-content > div:nth-child(3) > div > form > div > input");
+    input_element.setAttribute("name", search_links[event.currentTarget.id].param);
 })
